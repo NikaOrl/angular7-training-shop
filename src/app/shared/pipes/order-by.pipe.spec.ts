c2c86bba6a7a9081ffe1b75e9e2289d75f0b6788
Nika Orlova <nika.orlova.1997@yandex.ru>
2019-05-15 19:35:38 +0300

    unit tests added

--- src/app/shared/pipes/order-by.pipe.spec.ts
+++ src/app/shared/pipes/order-by.pipe.spec.ts
@@ -5,4 +5,19 @@ describe('OrderByPipe', () => {
     const pipe = new OrderByPipe();
     expect(pipe).toBeTruthy();
   });
+
+  it('should sort an array', () => {
+    const pipe = new OrderByPipe();
+    const testedArray = [
+      { product: { name: 2, price: 1 } },
+      { product: { name: 3, price: 2 } },
+      { product: { name: 1, price: 3 } },
+    ];
+    const expectedArray = [
+      { product: { name: 1, price: 3 } },
+      { product: { name: 2, price: 1 } },
+      { product: { name: 3, price: 2 } },
+    ];
+    expect(pipe.transform(testedArray, 'name')).toEqual(expectedArray);
+  });
 });
